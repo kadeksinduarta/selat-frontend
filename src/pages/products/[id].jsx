87dab185f027d142cb075062544d640f2e09a6cd
kadeksinduarta <sindu.arta@student.undiksha.ac.id>
2025-12-17 10:45:40 +0800

    update import class

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import MainLayout from "@/pages/layouts/MainLayout";
-import ProductCard from "@/pages/components/ProductCard";
+import ProductCard from "@/components/ProductCard";
 import { apiGet } from "@/utils/api";
 import { addToCart } from "@/utils/cart";
 import {
